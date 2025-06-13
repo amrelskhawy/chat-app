9b860231adb4db82ed6afa8b366b57fbfef59d42
Amr Elskhawy <amrelskhawy@gmail.com>
2025-06-13 23:12:10 +0300

    make user interface and model

---
+++ backend/models/user.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { IUser } from "../types/user.interface";
+
+const UserSchema: Schema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
